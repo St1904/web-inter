42d1015bd034888a2cede02bbe8d6d80fb72c698
St <_rammstanya@mail.ru>
2017-08-08 22:39:50 +0300

    В карточку студента добавлен вывод списка контактов

--- js/students.js
+++ js/students.js
@@ -1,4 +1,4 @@
-function show_students () {
+function show_students() {
     $.ajax({
         type: "GET",
         datatype: "json",
@@ -8,15 +8,29 @@ function show_students () {
         }
     }).then(function (data) {
         $.each(data, function () {
-            var panel = $("<div class=\"panel-primary\">\n" +
-                "            <div class=\"panel-heading\">\n" +
+            var id_student = this.id;
+            var panel = $(
+                "<div class=\"panel-primary\">" +
+                "            <div class=\"panel-heading\">" +
                                 this.firstName + " " + this.lastName +
-                "            </div>\n" +
-                "            <div class=\"panel-body\">\n" +
-                "                Инфо о студенте\n" +
-                "            </div>\n" +
-                "        </div>");
+                "            </div>" +
+                "            <div class=\"panel-body\">" +
+                "                Домашний адрес: " +
+                                this.address + "<br/>" +
+                "                Контакты: " +
+                "                <ul id='contacts" + id_student + "'></ul>" +
+                "            </div>" +
+                "        </div>"
+                )
+            ;
             $('#student-list').append(panel);
+            $.each(this.contacts, function () {
+                var contact_info = $(
+                    "<li>" +
+                    this.contactName.name + "  -  " + this.value +
+                    "</li>");
+                $('#contacts' + id_student).append(contact_info);
+            })
         })
     });
 }
